78b8ad74ab4df41df7915854b16e2b98eb1ad808
smoran3 <skmoran3@gmail.com>
2018-12-10 15:57:27 -0500

    troubleshooting repeated data

--- TransitLoads_FromPointToLine.py
+++ TransitLoads_FromPointToLine.py
@@ -63,6 +63,82 @@ for i in xrange(len(routes)):
 #table linking route/direction with shapes used
 route_dir_shape = zip(route_list, direction_list, selected_shapes)
 
+for i in xrange(0,len(route_dir_shape)):
+    cur.execute("""
+        INSERT INTO route_dir_shape (route, direction, shape)
+        VALUES (%s, %s, %s)
+        """,[route_dir_shape[i][0], route_dir_shape[i][1], route_dir_shape[i][2]])
+    con.commit()
+
+#create direction crosswalk table with first route/direction combo
+cur.execute("""
+    CREATE TABLE direction_crosswalk AS(
+        with _idea AS (
+            SELECT st.*, t.route_id,  t.direction_id, st_l.*
+            FROM gtfs_stop_times st
+            LEFT JOIN gtfs_trips t ON t.trip_id = st.trip_id
+            LEFT JOIN surfacetransit_loads st_l ON st_l.stop_id::text = st.stop_id AND st_l.route = t.route_id
+            WHERE st_l.route = '{0}'
+            AND t.direction_id = '{1}'
+        ),
+        tblA AS(
+            SELECT route_id, direction_id, direction, count(*) cnt 
+            FROM _idea
+            GROUP BY route_id, direction_id, direction
+            )
+
+        SELECT
+            route_id,
+            direction_id,
+            direction
+        FROM tblA
+        WHERE cnt = (
+            SELECT 
+                MAX(cnt)
+            FROM tblA)
+    )""".format(route_dir_shape[0][0], route_dir_shape[0][1]))
+con.commit()
+
+#skip first to start iterating at second value after table is created
+sub_routes = []
+for s in route_dir_shape[1:]:
+    sub_routes.append(s)
+    
+for i in xrange(len(sub_routes)):
+    cur.execute("""
+        with _idea AS (
+            SELECT st.*, t.route_id,  t.direction_id, st_l.*
+            FROM gtfs_stop_times st
+            LEFT JOIN gtfs_trips t ON t.trip_id = st.trip_id
+            LEFT JOIN surfacetransit_loads st_l ON st_l.stop_id::text = st.stop_id AND st_l.route = t.route_id
+            WHERE st_l.route = '{0}'
+            AND t.direction_id = '{1}'
+        ),
+        tblA AS(
+            SELECT route_id, direction_id, direction, count(*) cnt 
+            FROM _idea
+            GROUP BY route_id, direction_id, direction
+            )
+
+        SELECT
+            route_id,
+            direction_id,
+            direction
+        FROM tblA
+        WHERE cnt = (
+            SELECT 
+                MAX(cnt)
+            FROM tblA)
+    """.format(sub_routes[i][0], sub_routes[i][1]))
+    print sub_routes[i][0]
+    a = cur.fetchall()
+    if a != []:
+        cur.execute("""
+            INSERT INTO direction_crosswalk (route_id, direction_id, direction)
+            VALUES (%s, %s, %s)
+            """,[a[0][0], a[0][1], a[0][2]])
+    con.commit()
+
 #create table with first shape results
 cur.execute("""
         CREATE TABLE sequence_loads AS(
@@ -79,9 +155,9 @@ cur.execute("""
                 SELECT _stops.stop_id, _stop_times_agg.stop_sequence, ST_SetSRID(ST_MakePoint(stop_lon, stop_lat), 4326) geom 
                 FROM _stops 
                 INNER JOIN (
-                SELECT stop_id, MIN(stop_sequence) stop_sequence 
-                FROM _stop_times 
-                GROUP BY stop_id) _stop_times_agg 
+                    SELECT stop_id, MIN(stop_sequence) stop_sequence 
+                    FROM _stop_times 
+                    GROUP BY stop_id) _stop_times_agg 
                 ON _stops.stop_id = _stop_times_agg.stop_id 
                 ORDER BY stop_sequence
                 ),
@@ -105,17 +181,29 @@ cur.execute("""
                 ),
             sequence_limits AS(
                 SELECT * FROM _shapes_xt13 WHERE rank = 1 ORDER BY shape_pt_sequence
+                ),
+            direction_join AS(
+                SELECT r.*, d.direction AS direc
+                FROM route_dir_shape r
+                INNER JOIN direction_crosswalk d
+                ON r.route = d.route_id AND r.direction = d.direction_id::text
+                ), 
+            anotherjoin AS(
+                SELECT s.stop_id, s.shape_id, s.shape_pt_sequence, d.route, d.direction, d.direc
+                FROM sequence_limits s
+                INNER JOIN direction_join d
+                ON s.shape_id = d.shape
                 )
-            SELECT 
-                s.stop_id,
-                s.shape_id,
-                s.shape_pt_sequence, 
-                sum(b.weekday_lo) as tot_loads
-            FROM sequence_limits s
-            INNER JOIN surfacetransit_loads b
-            ON s.stop_id = b.stop_id::text
-            GROUP BY s.stop_id, s.shape_id, s.shape_pt_sequence, s.geom, s.length, s.rank
-            ORDER BY shape_pt_sequence
+            SELECT
+            a.*,
+            sum(s.weekday_lo) as tot_loads
+            FROM anotherjoin a
+            INNER JOIN surfacetransit_loads s
+            ON a.stop_id = s.stop_id::text
+            AND a.route = s.route
+            AND a.direc = s.direction
+            GROUP BY a.stop_id, a.shape_id, a.shape_pt_sequence, a.route, a.direction, a.direc
+            ORDER BY a.shape_pt_sequence
             );
         """.format(selected_shapes[0]))
 con.commit()
@@ -167,25 +255,37 @@ for i in xrange(len(sub_shapes)):
             ),
         sequence_limits AS(
             SELECT * FROM _shapes_xt13 WHERE rank = 1 ORDER BY shape_pt_sequence
+            ),
+        direction_join AS(
+            SELECT r.*, d.direction AS direc
+            FROM route_dir_shape r
+            INNER JOIN direction_crosswalk d
+            ON r.route = d.route_id AND r.direction = d.direction_id::text
+            ), 
+        anotherjoin AS(
+            SELECT s.stop_id, s.shape_id, s.shape_pt_sequence, d.route, d.direction, d.direc
+            FROM sequence_limits s
+            INNER JOIN direction_join d
+            ON s.shape_id = d.shape
             )
-        SELECT 
-            s.stop_id,
-            s.shape_id,
-            s.shape_pt_sequence, 
-            sum(b.weekday_lo) as tot_loads
-        FROM sequence_limits s
-        INNER JOIN surfacetransit_loads b
-        ON s.stop_id = b.stop_id::text
-        GROUP BY s.stop_id, s.shape_id, s.shape_pt_sequence, s.geom, s.length, s.rank
-        ORDER BY shape_pt_sequence
+        SELECT
+        a.*,
+        sum(s.weekday_lo) as tot_loads
+        FROM anotherjoin a
+        INNER JOIN surfacetransit_loads s
+        ON a.stop_id = s.stop_id::text
+        AND a.route = s.route
+        AND a.direc = s.direction
+        GROUP BY a.stop_id, a.shape_id, a.shape_pt_sequence, a.route, a.direction, a.direc
+        ORDER BY a.shape_pt_sequence
         """.format(sub_shapes[i]))
         print sub_shapes[i]
         results = cur.fetchall()
         for row in results:
             cur.execute("""
-                INSERT INTO sequence_loads (stop_id, shape_id, shape_pt_sequence, tot_loads)
-                VALUES ({0}, {1}, {2}, {3})
-            """.format(row[0], row[1], row[2], row[3]))
+                INSERT INTO sequence_loads (stop_id, shape_id, shape_pt_sequence, route, direction, direc, tot_loads)
+                VALUES (%s, %s, %s, %s, %s, %s, %s)
+            """,[row[0], row[1], row[2], row[3], row[4], row[5], row[6]])
         con.commit()
         
         
